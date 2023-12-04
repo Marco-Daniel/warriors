eeb2c1d11feffac7c7224d7360a2e5119ab7004f
Marco Daniël Leguijt <m.leguijt@outlook.com>
2023-12-04 22:04:54 +0100

    feature: start basic character animation

--- src/player.ts
+++ src/player.ts
@@ -1,4 +1,4 @@
-import { Actor, vec } from "excalibur";
+import { Actor, vec, Animation, Vector } from "excalibur";
 import { Resources } from "./resources";
 
 export class Player extends Actor {
@@ -6,14 +6,28 @@ export class Player extends Actor {
     super({
       pos: vec(150, 150),
       width: 100,
-      height: 100
+      height: 100,
     });
   }
 
   onInitialize() {
-    this.graphics.add(Resources.Sword.toSprite());
-    this.on('pointerup', () => {
-      alert('yo');
+    // Setup visuals
+    const idle = Animation.fromSpriteSheet(
+      Resources.wandererMagicanSprites.idle,
+      [0, 1, 2, 3, 4],
+      150
+    );
+    idle.scale = new Vector(2, 2);
+
+    this.graphics.add("idle", idle);
+
+    // test
+    this.on("pointerup", () => {
+      alert("yo");
     });
+  }
+
+  onPreUpdate(engine: ex.Engine, delta: number) {
+    this.graphics.use("idle");
   }
 }
