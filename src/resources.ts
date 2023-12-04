07c5166b0dbb44620776065c3227827562809d7c
Marco Daniël Leguijt <m.leguijt@outlook.com>
2023-12-04 22:03:28 +0100

    style: include spites in engine

--- src/resources.ts
+++ src/resources.ts
@@ -1,8 +1,37 @@
-import { ImageSource } from "excalibur";
+import { ImageSource, Loader, SpriteSheet } from "excalibur";
 import sword from "./images/sword.png";
+import wandererMagicanIdle from "./images/Wanderer_Magican/Idle.png";
 
-let Resources = {
-  Sword: new ImageSource(sword)
+const resources = {
+  Sword: new ImageSource(sword),
 };
 
-export { Resources };
+const wandererMagicanImages = {
+  idle: new ImageSource(wandererMagicanIdle),
+};
+
+const wandererMagicanSprites = {
+  idle: SpriteSheet.fromImageSource({
+    image: wandererMagicanImages.idle,
+    grid: {
+      columns: 5,
+      rows: 1,
+      spriteWidth: 75,
+      spriteHeight: 125,
+    },
+    spacing: {
+      margin: {
+        x: 195,
+      },
+    },
+  }),
+};
+
+const loader = new Loader([
+  ...Object.values(wandererMagicanImages),
+  ...Object.values(resources),
+]);
+
+const Resources = { wandererMagicanSprites, wandererMagicanImages };
+
+export { loader, Resources };
